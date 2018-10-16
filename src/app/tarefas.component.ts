a6d3a268b532716c56837ff4cd85f5b7b995af9f
pietropr <pintonpietro@gmail.com>
2018-10-16 18:53:00 -0300

    Commit

--- src/app/tarefas.component.ts
+++ src/app/tarefas.component.ts
@@ -2,13 +2,35 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'tarefas',
-	templateUrl: './tarefas.component.html'
+	templateUrl: './tarefas.component.html',
+	styles: [
+
+	`
+
+		button {
+		    border-radius: 0;
+		    padding: 5px 20px;
+		    font-weight: 700;
+		    color: #fff;
+		    box-shadow: none;
+		    border: 0;
+		    background: #8a2be2;
+		    cursor: pointer;
+		    margin-right: 20px;	
+		}
+
+	`
+
+	]
 })
 export class TarefasComponent {
 	tarefas: string[];
 	n: number = 0;
 	incrementa() {
 		this.n++;
+	}
+	zerar() {
+		this.n = 0;
 	}
 	
 	constructor() {
